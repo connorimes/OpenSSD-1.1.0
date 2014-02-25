fbc443a225b0b25ad31a22847889454ad82fc085
Connor Imes <connor.k.imes@gmail.com>
2014-02-25 21:04:21 +0000

    Updated test_latencies with working read/write

--- test_latencies.c
+++ test_latencies.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 
 #define BLOCK_SIZE 512
@@ -13,27 +14,77 @@ char* create_data(int size, char* data) {
 	*data = '\0';
 }
 
+int write_dev(const int f, const int size, const int offset) {
+	if (lseek(f, offset, SEEK_SET) < 0) {
+		perror("Failed to seek");
+		return -1;
+	}
+
+	char* data;
+	// must align the memory for DMA
+	if(posix_memalign((void**)&data, BLOCK_SIZE, size) < 0) {
+		perror("Failed to align memory");
+		return -1;
+	}
+
+	create_data(size, data);
+	// printf("%s\n", data);
+
+	printf("Writing to device\n");
+	const int c_wrote = write(f, data, size);
+	printf("Wrote %d characters\n", c_wrote);
+
+	// cleanup from malloc in posiz_memalign
+	free(data);
+	return c_wrote;
+}
+
+int read_dev(const int f, const int size, const int offset) {
+	if (lseek(f, offset, SEEK_SET) < 0) {
+		perror("Failed to seek");
+		return -1;
+	}
+
+	printf("Reading from device\n");
+	int remaining = size;
+	int c_read = 0;
+	char buf[BLOCK_SIZE];
+	while (remaining > 0) {
+		remaining -= BLOCK_SIZE;
+		// make sure we don't read too much
+		const int r_size = remaining >= 0 ? BLOCK_SIZE : BLOCK_SIZE + remaining;
+		const int curr_read = read(f, buf, r_size);
+		c_read += curr_read;
+		if (curr_read != r_size) {
+			fprintf(stderr, "Failed to read block\n");
+		}
+		// printf("%s\n", buf);
+	}
+	printf("Read %d characters\n", c_read);
+	return c_read;
+}
+
 /*
 * Pass the block device location, offset, and data size as the command line arguments.
 */
-int main(int argc, char* argv[] )
+int main(const int argc, const char* argv[] )
 {
 	if (argc < 4) {
 		fprintf(stderr, "Must pass device path, offset, and data size as arguments\n");
 		return -1;
 	}
 	// get block device to use
-	char* dev = argv[1];
+	const char* dev = argv[1];
 	printf("Device is: %s\n", dev);
 
-	int offset = atoi(argv[2]);
+	const int offset = atoi(argv[2]);
 	if (offset < 0) {
 		printf("Offset must be > 0\n");
 		return -1;
 	}
 	printf("Offset is: %d\n", offset);
 
-	int size = atoi(argv[3]);
+	const int size = atoi(argv[3]);
 	if (size < 1) {
 		printf("Size must be greater than 0\n");
 		return -1;
@@ -45,31 +96,27 @@ int main(int argc, char* argv[] )
 		perror("Device not found or cannot read/write");
 		return -1;
 	}
-	// char data[size];
-	char* data;
-	if(posix_memalign((void**)&data, BLOCK_SIZE, size) < 0) {
-		perror("Failed to align memory");
-		return -1;
-	}
-	create_data(size, data);
-	// printf("%s\n", data);
 
 	// open block device
-	int f = open(dev, "rw");
-
+	const int f = open(dev, O_RDWR | O_NONBLOCK);
 	if (f < 0) {
-		fprintf(stderr, "Error opening block device %s", dev);
-	} else {
-		printf("Writing to device\n");
-		lseek(f, offset, SEEK_SET);
-		int wrote = write(f, data, size);
-		printf("Wrote %d characters\n", wrote);
-		//fsync(f);
-		// don't forget to close
-		printf("Closing device\n");
-		close(f);
+		fprintf(stderr, "Error opening block device %s\n", dev);
+		return -1;
 	}
-	free(data);
-	data = NULL;
-	return(0);
+
+	const int c_wrote = write_dev(f, size, offset);
+	if (c_wrote != size) {
+		fprintf(stderr, "Write size did not match requested size\n");
+	}
+	const int c_read = read_dev(f, size, offset);
+	if (c_read != size) {
+		fprintf(stderr, "Read size did not match requested size\n");
+	}
+	// don't forget to close
+	printf("Closing device\n");
+	if(close(f) < 0) {
+		perror("Failed to close device file");
+	}
+
+	return 0;
 }
